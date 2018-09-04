1c0fda5018df8078c6de652d37a6fd91ce7d6070
michaelkochub <michaelkochub@gmail.com>
2018-09-03 22:42:36 -0400

    Add container components to complete app UI

---
+++ src/containers/VisibleTodoList.js
@@ -0,0 +1,36 @@
+import TodoList from "../components/TodoList";
+import { VisibilityFilters, toggleTodo } from "../actions"
+import { connect } from 'react-redux'
+
+const getVisibleTodos = (todos, filter) => {
+    switch (filter) {
+        case VisibilityFilters.SHOW_COMPLETED:
+            return todos.filter(t => t.completed)
+        case VisibilityFilters.SHOW_ACTIVE:
+            return todos.filter(t => !t.completed)
+        case VisibilityFilters.SHOW_ALL:
+        default:
+            return todos
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        todos: getVisibleTodos(state.todos, state.visibilityFilter)
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onTodoClick: id => {
+            dispatch(toggleTodo(id))
+        }
+    }
+}
+
+const VisibleTodoList = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(TodoList)
+
+export default VisibleTodoList
--- src/index.js
+++ src/index.js
@@ -1,25 +1,15 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import todoApp from './reducers'
-import {
-    addTodo,
-    toggleTodo,
-    VisibilityFilters, 
-    setVisibilityFilter
-} from './actions'
+import App from './components/App'
 
 const store = createStore(todoApp)
 
-console.log(store.getState())
-
-const unsubscribe = store.subscribe(() =>
-    console.log(store.getState())
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 )
-
-store.dispatch(addTodo('Learn about actions'))
-store.dispatch(addTodo('Learn about reducers'))
-store.dispatch(addTodo('Learn about store'))
-store.dispatch(toggleTodo(0))
-store.dispatch(toggleTodo(1))
-store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
-
-unsubscribe()
